446c421c27d1ed182227d50b0bc4a648d82cfe16
Sanchay Singh <sanchay072@gmail.com>
2025-01-26 17:54:31 +0530

    nav wip

--- index.js
+++ index.js
@@ -8,13 +8,46 @@ function swapColors() {
 }
 
 let dark = true;
+const theme = document.getElementsByClassName("theme")[0];
+const menu = document.getElementById("hamburger");
+const modal = document.getElementById("modal");
+const modalLinks = document.querySelectorAll(".ham");
 
-document.getElementById("theme").addEventListener("click",()=>{
+let close = false;
+
+theme.addEventListener("click",()=>{
+    theme.classList.add("bounce");
+
+    theme.addEventListener("animationend",()=>{
+        theme.classList.remove("bounce");
+    });
+    
     if(dark) {
-        document.getElementById("theme").innerHTML='<span class="material-symbols-outlined">dark_mode</span>';
+        theme.innerHTML='<span class="material-symbols-outlined">dark_mode</span>';
     }else{
-        document.getElementById("theme").innerHTML='<span class="material-symbols-outlined">light_mode</span>';
+        theme.innerHTML='<span class="material-symbols-outlined">light_mode</span>';
     }
     swapColors();
     dark=!dark;
+});
+
+menu.addEventListener("click",()=>{
+
+    if(!close){
+        modal.style.display = "flex";
+        menu.innerHTML = "×";
+        menu.style.zIndex = 3;
+    }else{
+        modal.style.display = "none";
+        menu.innerHTML = '<span class="material-symbols-outlined">menu</span>';
+    }
+
+    close=!close;
+    
+});
+
+modalLinks.forEach(element => {
+    element.addEventListener("click",()=>{
+        modal.style.display = "none";
+    });
 });
